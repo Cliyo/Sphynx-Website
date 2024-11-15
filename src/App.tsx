2fbaa6c5880a001816ba101cab558c08d70b521f
PedroVidalDev <pedrohvidals@gmail.com>
2024-11-15 16:27:59 -0300

    feat: add navigation bar component in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider } from "styled-components"
+
+import { NavigationBar } from "components/NavigationBar";
 import { theme } from "themes";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-
+      <NavigationBar />
     </ThemeProvider>
   );
 }

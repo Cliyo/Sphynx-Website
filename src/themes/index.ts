b08ab50f7b9fdef9fe25b11b25a850ba8f82656e
PedroVidalDev <pedrohvidals@gmail.com>
2024-11-15 15:13:36 -0300

    feat: add default themes for project and typing it

---
+++ src/themes/index.ts
@@ -0,0 +1,17 @@
+export const theme = {
+    COLORS: {
+        PRIMARY: "#003845",
+        NEUTRAL_0: "#FAFAFA",
+        NEUTRAL_300: "#D2D2D2",
+        NEUTRAL_500: "#3E3E3E",
+        NEUTRAL_700: "#232323",
+        NEUTRAL_900: "#1C1C1C"
+    },
+    FONT_SIZE: {
+        XSMALL: 24,
+        SMALL: 32,
+        REGULAR: 36,
+        LARGE: 40,
+        XLARGE: 96
+    }
+}

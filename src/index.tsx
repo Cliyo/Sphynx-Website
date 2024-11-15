24869f2db6c711740632ebe64b90519d3f58ac90
PedroVidalDev <pedrohvidals@gmail.com>
2024-11-15 15:14:14 -0300

    feat: add themes and translations in the project

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { GlobalStyle } from './themes/globalStyle';
+
+import './i18n'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );

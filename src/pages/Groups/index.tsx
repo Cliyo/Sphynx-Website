64644784e73699390d2f98dbcf2995e4f7632b59
PedroVidalDev <pedrohvidals@gmail.com>
2024-12-08 16:48:23 -0300

    feat: create function for find all groups

--- src/pages/Groups/index.tsx
+++ src/pages/Groups/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 import { NavLink } from "react-router-dom"
 
 import { groupsTableHeaders } from "constants/table"
@@ -9,36 +10,27 @@ import { Table } from "components/Table"
 import { Button } from "components/Button"
 
 import { Container, InputsContainer, Title } from "./styles"
+import { useGroup } from "hooks/useGroup"
 
 export const Groups = () => {
 
-    const groupsTableData: GroupsTableData[] = [
-      {
-            id: 1,
-            groupName: "Professor"
-        
-      },
-      {
-            id: 2,
-            groupName: "Aluno"
-      },
-      {
-            id: 3,
-            groupName: "Diretor"
-      },
-    ];
-      
-    return (
-        <Container>
-            <Title> Grupos </Title>
-            <InputsContainer>
-                <Input placeholder="Digite aqui..." />
-                <Button text="Filtrar" width={90}/>
-                <NavLink to={"/groups/new"}>
-                  <Button text="Criar" width={90} />
-                </NavLink>
-            </InputsContainer>
-            <Table headers={groupsTableHeaders} content={groupsTableData}/>
-        </Container>
-    )
+  const { groupPageData, fetchGetAllGroups } = useGroup();
+
+  useEffect(() => {
+    fetchGetAllGroups()
+  }, [])
+    
+  return (
+      <Container>
+          <Title> Grupos </Title>
+          <InputsContainer>
+              <Input placeholder="Digite aqui..." />
+              <Button text="Filtrar" width={90}/>
+              <NavLink to={"/groups/new"}>
+                <Button text="Criar" width={90} />
+              </NavLink>
+          </InputsContainer>
+          <Table headers={groupsTableHeaders} content={groupPageData}/>
+      </Container>
+  )
 }

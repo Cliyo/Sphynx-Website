51a9d49deb60d7965b765ac9e6bfe0fe9025afd1
PedroVidalDev <pedrohvidals@gmail.com>
2024-12-31 18:53:51 -0300

    refactor: alter redirect in delete request

--- src/hooks/useCustomer.tsx
+++ src/hooks/useCustomer.tsx
@@ -43,7 +43,7 @@ export const useCustomer = () => {
     const fetchDeleteCustomerById = async (id: string) => {
         await app.delete(`/consumers/${id}`)
 
-        navigate('/costumers')
+        navigate('/customers')
     }
 
     return {

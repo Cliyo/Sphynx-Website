dd7d236f46b1f76a18aab66f72bd318caafb8401
PedroVidalDev <pedrohvidals@gmail.com>
2025-01-09 17:09:28 -0300

    feat: create hook for auth

---
+++ src/hooks/useAuth.tsx
@@ -0,0 +1,19 @@
+import { useNavigate } from "react-router-dom"
+
+import { app } from "services/axios"
+
+import { LoginFormData } from "pages/Login/types"
+
+export const useAuth = () => {
+    const navigate = useNavigate()
+
+    const fetchLogin = async (data: LoginFormData) => {
+        await app.post('/auth/login', data)
+
+        navigate('/dashboard')
+    }
+
+    return {
+        fetchLogin
+    }
+}

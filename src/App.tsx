79b7fb2393e4a2c7c079eb7e84dc5ad809a94f08
PedroVidalDev <pedrohvidals@gmail.com>
2025-01-09 15:49:36 -0300

    feat: create alert modal context, to use the modal in every local of the website

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import { ThemeProvider } from "styled-components"
 import { NavigationBar } from "components/NavigationBar";
 import { theme } from "themes";
 import { RouteApp } from "Routes";
+import { AlertContextProvider } from "contexts/AlertContext";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <RouteApp />
+      <AlertContextProvider>
+        <RouteApp />
+      </AlertContextProvider>
     </ThemeProvider>
   );
 }
---
+++ src/contexts/AlertContext/index.tsx
@@ -0,0 +1,42 @@
+import ReactModal from "react-modal"
+import React, { createContext, useState } from "react"
+
+import { globalAlertStyles } from "themes/globalAlertStyles"
+
+import { AlertContextDataProps, AlertContextProviderProps } from "./types"
+
+import { Modal } from "components/Modal"
+import { ModalProps } from "components/Modal/types"
+
+export const AlertContext = createContext({} as AlertContextDataProps)
+
+export const AlertContextProvider = ({children}: AlertContextProviderProps) => {
+    const [modalIsOpen, setIsOpen] = useState(false)
+    const [modalProps, setModalProps] = useState<ModalProps>({} as ModalProps)
+
+    const { title, message, onConfirm } = modalProps
+
+    const handleToggleModal = () => {
+        setIsOpen(!modalIsOpen)
+    }
+
+    const onConfirmModal = () => {
+        onConfirm()
+        handleToggleModal()
+    }
+
+    const alert = (props: ModalProps) => {
+        setModalProps(props)
+        handleToggleModal()
+    }
+
+    return (
+        <AlertContext.Provider value={{alert}}>
+            {children}
+
+            <ReactModal isOpen={modalIsOpen && !!modalProps} style={globalAlertStyles}>
+                <Modal title={title} message={message} onClose={handleToggleModal} onConfirm={onConfirmModal} />
+            </ReactModal>
+        </AlertContext.Provider>
+    )
+}

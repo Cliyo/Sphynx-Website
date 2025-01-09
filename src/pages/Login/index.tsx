71045ff6db3bac3355859b0e3aa443294820bb2d
PedroVidalDev <pedrohvidals@gmail.com>
2025-01-09 16:58:20 -0300

    feat: create login page component and style

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,76 @@
+import { useTranslation } from "react-i18next"
+import { Controller, useForm } from "react-hook-form"
+
+import { Button } from "components/Button"
+import { Input } from "components/Input"
+
+import { LoginFormData } from "./types"
+
+import { REGEX } from "constants/regex"
+
+import { Banner, Container, ContainerForm, Form, Image } from "./styles"
+
+export const Login = () => {
+
+    const { t } = useTranslation()
+
+    const {control, handleSubmit, formState: {errors} } = useForm<LoginFormData>({
+        defaultValues: {
+            email: '',
+            password: ''
+        }
+    })
+
+    const onSubmit = (data: LoginFormData) => {
+        console.log("enviou: ", data)
+    }
+
+    return (
+        <Container>
+            <Banner>
+                <Image />
+            </Banner>
+            <ContainerForm>
+                <Form>
+                    <Controller
+                        control={control}
+                        name="email"
+                        rules={{
+                            required: t('inputErrors.required'),
+                            pattern: {
+                                value: REGEX.onlyString,
+                                message: t('inputErrors.text')
+                            }
+                        }}
+                        render={({field: {onChange, value}}) => (
+                            <Input
+                                value={value}
+                                onChange={onChange}
+                                placeholder={t('placeholder.default')}
+                                label={t('inputLabel.email')}
+                                errorMessage={errors.email?.message}
+                            />
+                        )}
+                    />
+                    <Controller
+                        control={control}
+                        name="password"
+                        rules={{
+                            required: t('inputErrors.required')
+                        }}
+                        render={({field: {onChange, value}}) => (
+                            <Input
+                                value={value}
+                                onChange={onChange}
+                                placeholder={t('placeholder.default')}
+                                label={t('inputLabel.password')}
+                                errorMessage={errors.password?.message}
+                            />
+                        )}
+                    />
+                    <Button onClick={handleSubmit(onSubmit)} text={t('button.login')} height={50} />
+                </Form>
+            </ContainerForm>
+        </Container>
+    )
+}
---
+++ src/pages/Login/styles.ts
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+
+    flex: 1;
+    height: 100%;
+
+    justify-content: space-between;
+`
+
+export const Banner = styled.div`
+    display: flex;
+
+    height: 100%;
+    width: 60%;
+
+    justify-content: center;
+    align-items: center;
+
+    background-color: ${({theme}) => theme.COLORS.PRIMARY_DARK};
+`
+
+export const Image = styled.img`
+    width: 100%;
+    height: 100%;
+`
+
+export const ContainerForm = styled.div`
+    display: flex;
+
+    height: 100%;
+    flex: 1;
+
+    justify-content: center;
+    align-items: center;
+`
+
+export const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+
+    width: 70%;
+
+    gap: 30px;
+`

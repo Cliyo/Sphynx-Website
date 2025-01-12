82cfc8005ad80f88a9b3b222f7b1fa6f90696fd7
PedroVidalDev <pedrohvidals@gmail.com>
2025-01-12 14:25:24 -0300

    feat: add icon in not found page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from 'components/Button'
 import { Container, ErrorContainer, Text, Title } from './styles'
 import { useTranslation } from 'react-i18next'
+import { Icon } from 'components/Icon'
 
 export const NotFound = () => {
   const { t } = useTranslation()
@@ -12,6 +13,7 @@ export const NotFound = () => {
   return (
     <Container>
       <ErrorContainer>
+        <Icon color="PRIMARY_DARK" size="124" name="IoSkullOutline" />
         <Title> 404 </Title>
         <Text> {t('httpErrors.404')} </Text>
         <Button text="Voltar" height={50} onClick={handleBack} />
